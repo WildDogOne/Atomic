98cad4358dcd4bc67939331097a3c105a695f92e
Läubli Linus (laei) <laei@zhaw.ch>
2023-04-19 13:33:46 +0200

    testing with lolbas

--- custom_attack.ps1
+++ custom_attack.ps1
@@ -1,1 +1,8 @@
-Invoke-AtomicTest All -PathToAtomicsFolder C:\AtomicRedTeam\atomics -ShowDetailsBrief
+$ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
+. ".\config.ps1"
+$psCred = New-Object System.Management.Automation.PSCredential -ArgumentList ($username, $password)
+$sess = New-PSSession -ComputerName $target -Credential $psCred
+#Invoke-AtomicTest All -PathToAtomicsFolder $ScriptDir\atomics -ShowDetailsBrief
+#Invoke-AtomicTest All -PathToAtomicsFolder .\atomics -ShowDetailsBrief
+Invoke-AtomicTest -TimeoutSeconds 60 test -Session $sess -PathToAtomicsFolder .\atomics
+Remove-PSSession $sess
